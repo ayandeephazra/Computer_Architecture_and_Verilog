29eed53607b83cc1da30704f29b03786a9738b61
Ayandeep Hazra <ayandeephazra@hotmail.com>
2021-11-23 00:36:47 -0600

    HW5 I and D changes fixed!!!!

--- ECE551-Digital System Design and Synthesis/HW5/P1/Dterm.sv
+++ ECE551-Digital System Design and Synthesis/HW5/P1/Dterm.sv
@@ -1,42 +1,46 @@
 module D_term(err_sat, err_vld, rst_n, clk, D_term);
 
-input [9:0] err_sat;
-input err_vld, rst_n, clk;
-output [12:0] D_term;
+        input [9:0] err_sat;
+	input err_vld, rst_n, clk;
+	logic signed [12:0] D_term;
 
-logic [9:0] mux1_out, mux2_out, D_diff, prev_err;
-reg [9:0] q1, q2;
-logic [6:0] saturated_D_diff;
+	logic [9:0] mux1_out, mux2_out, D_diff, prev_err;
+	reg [9:0] q1, q2;
+	logic signed [6:0] saturated_D_diff;
 
-localparam P_COEFF = 6'h0B;
+	localparam P_COEFF_DTERM = 6'h0B;
 
-assign mux1_out = (err_vld)? err_sat: q1;
+	assign mux1_out = (err_vld)? err_sat[9:0]: q1;
 
-// FF 1
-always_ff @(posedge clk)
-if(!rst_n)
-q1 <= 10'h000;
-else
-q1 <= mux1_out;
+	// FF 1
+	always_ff @(posedge clk, negedge rst_n)
+	if(!rst_n)
+	q1 <= 10'h000;
+	else
+	q1 <= mux1_out;
 
-assign mux2_out = (err_vld)? q1: q2;
+	assign mux2_out = (err_vld)? q1: q2;
 
-// FF 2
-always_ff @(posedge clk)
-if(!rst_n)
-q2 <= 10'h000;
-else
-q2 <= mux2_out;
+	// FF 2
+	always_ff @(posedge clk, negedge rst_n)
+	if(!rst_n)
+	q2 <= 10'h000;
+	else
+	q2 <= mux2_out;
 
-assign prev_err = q2;
+	assign prev_err = q2;
 
-//assign D_diff = err_sat - prev_err;
-assign D_diff = err_sat - q2;
+	//assign D_diff = err_sat - prev_err;
+	assign D_diff = err_sat[9:0] - q2;
 
-// Saturation
-assign saturated_D_diff = (D_diff[9]===1)? ((D_diff[8:7]===2'b11)?D_diff[9:0]:6'h20) :
-                          ((D_diff[8:7]===2'b00)?D_diff[9:0]:6'h1F) ;
+	// Saturation
+	assign saturated_D_diff = (D_diff[9]==1)? ((D_diff[8:6]==3'b111)?D_diff[6:0]:7'h40) :
+							  ((D_diff[8:6]==3'b000)?D_diff[6:0]:7'h3f) ;
 
-assign D_term = saturated_D_diff * $signed(P_COEFF);
+							
+							  
+	// ERR VLD ASSERTION
+	//assign D_term = (err_vld)?saturated_D_diff * $signed(P_COEFF_DTERM):D_term;
+	assign D_term = saturated_D_diff * $signed(P_COEFF_DTERM);
 
 endmodule
--- ECE551-Digital System Design and Synthesis/HW5/P1/I_term.sv
+++ ECE551-Digital System Design and Synthesis/HW5/P1/I_term.sv
@@ -2,33 +2,36 @@ module I_term(clk, rst_n, err_vld, moving, err_sat, I_term);
 
 input clk, rst_n, err_vld, moving;
 input signed [9:0]err_sat;
-output signed [8:0]I_term;
-logic overflow;
-// Registers required
-reg signed [14:0]nxt_integrator ,integrator, accum, mux1_out, err_sat_15;
 
-// Sign-extend to 15 bits
-assign err_sat_15 = {{5{err_sat[9]}},err_sat};
+	logic signed [8:0]I_term;
+	logic overflow;
+	// Registers required
+	reg signed [14:0]nxt_integrator ,integrator, accum, mux3_out, err_sat_15;
 
-//Accumulator
-assign accum = (integrator + err_sat_15);
+	// Sign-extend to 15 bits
+	assign err_sat_15 = {{5{err_sat[9]}},err_sat};
 
-// Overflow statement
-assign overflow = ( (err_sat_15[14] === integrator[14]) && (integrator[14] === accum[14]) ) ? 1'b0 : 1'b1;
+	//Accumulator
+	assign accum = (integrator + err_sat_15);
 
-// Check error valid and overflow
-assign mux1_out = (err_vld && (!overflow)) ? accum: integrator ;
+	// Overflow statement
+	assign overflow = ( (err_sat_15[14] == integrator[14]) && (integrator[14] != accum[14]) ) ? 1'b1 : 1'b0;
 
-//Check moving and assign to nxt_integrator
-assign nxt_integrator = (moving) ?  mux1_out : 15'h0000 ;
+	// Check error valid and overflow
+	assign mux3_out = (err_vld && (!overflow)) ? accum: integrator ;
 
-always_ff @(posedge clk, negedge rst_n)
-if(!rst_n)
-  integrator<= 15'h0000;
-else
-  integrator<= nxt_integrator;
+	//Check moving and assign to nxt_integrator
+	assign nxt_integrator = (moving) ?  mux3_out : 15'h0000 ;
 
-//I_term
-assign I_term =  integrator[14:6];
+	always_ff @(posedge clk, negedge rst_n)
+	if(!rst_n)
+	  integrator<= 15'h0000;
+	else
+	  integrator<= nxt_integrator;
+
+	//I_term
+	// only when err_vld is high
+	//assign I_term =  (err_vld)?integrator[14:6]:I_term;
+    assign I_term = integrator[14:6];
 
 endmodule
